a103522871e7e19a65e3e5c4db46a0910ac9353e
mukulpal03 <167329255+mukulpal03@users.noreply.github.com>
2024-05-23 14:42:15 +0000

    functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -29,3 +29,40 @@ function loginUserMessage(username = "sam") {  // "sam" => we gave a default val
 console.log(loginUserMessage("mukul"));
 
 // console.log(loginUserMessage());  // what if you don't pass any value, it will return undefined.
+
+// aage jaake we'll make some projects and unn projects m mostly ek shopping cart hoti h, toh shopping cart mein user keeps on adding stuff toh hume pata nahi hota kitne number of arguments aane wale hai, hume bas sabki price add karni hoti h. it is a situation where we don't know how many arguments will be there.
+
+// so to deal with this situation
+
+
+function calculateCartPrice(...num1) {  // this is called rest operator, isse hi rest operator bolte hai and isse hi spread operator bhi, sirf usecase pe depend karta h
+    return num1;
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000)); // now it'll take every argument, otherwise it was just taking the first one.
+
+// function calculateCartPrice(val1, val2, ...num1) // now it'll take 200 and 400 as val1 & val2 and rest as num1.
+
+const user = {
+    username: "mukul",
+    price: 199
+};
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user);
+
+handleObject({               // you can pass explicitly also
+    username: "sam",
+    price: 499
+})
+
+const myNewArray = [200, 400, 100, 600];
+
+function handleArray(getArray) {
+    return getArray[1];
+}
+
+console.log(handleArray(myNewArray));
