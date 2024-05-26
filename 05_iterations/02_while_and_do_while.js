86fca019ec6f7c8ce29bab23869e506b703cd7e4
mukulpal03 <167329255+mukulpal03@users.noreply.github.com>
2024-05-26 16:20:08 +0000

    While loop

---
+++ 05_iterations/02_while_and_do_while.js
@@ -0,0 +1,23 @@
+let i = 0;
+
+while(i<=10) {
+    // console.log(i);
+    i++
+}
+
+const myArr = ["flash","batman","hulk"]
+let index = 0;
+
+while (index<myArr.length) {
+    console.log(myArr[index]);
+    index++;
+}
+
+// do while loop 
+
+let score = 11;
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
